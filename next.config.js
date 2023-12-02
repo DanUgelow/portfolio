c8ff389a3e7de2f44b20318cb1888749c44d1f34
Dan Ugelow <dan.ugelow@gmail.com>
2023-12-02 16:54:42 -0500

    fix broken paths

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  reactStrictMode: true,
+  // Add basePath
+  basePath: "/github-pages",
 };
 
 module.exports = nextConfig;
